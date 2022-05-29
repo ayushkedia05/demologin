ce3ef2ca549a491ad8c2411ea670f2884158779f
Ayush <kediaayush26@gmail.com>
2022-05-29 17:53:25 +0530

    push

--- app.js
+++ app.js
@@ -3,17 +3,32 @@ const morgan=require('morgan');
 const userRoutes =require('./routes/userroutes.js')
 let cors=require("cors");
 const cookieParser = require('cookie-parser');
-
+const path=require('path');
 const app=express();
 app.use(cors());
 
 
-if(process.env.NODE_ENV==='development')
+if(process.env.NODE_ENV==='production')
 {
-    app.use(morgan('dev'));
+    app.use(express.static(path.join(__dirname,'userlogin/build')))
+    
+    
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'userlogin','build','index.html'));
+    })
+    
+}else {
+    app.get("/",(req,res)=>{
+        res.send("APi running");
+    })
 }
 
+
+
+app.use(morgan('dev'));
 app.use(cookieParser());
+
+
 
 
 app.use(express.json());
